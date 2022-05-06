1b900f076d9e49edb2ade0a6c9bdd3dcf4951e60
james <dlrjsdn53@naver.com>
2022-05-06 11:01:24 +0900

    2022.05.06 (금) 일정

--- src/main/java/com/owl/heybuddy/modules/plan/PlanController.java
+++ src/main/java/com/owl/heybuddy/modules/plan/PlanController.java
@@ -1,36 +1,33 @@
 package com.owl.heybuddy.modules.plan;
 
-import java.util.Locale;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PlanController {
-	
 
-	@RequestMapping(value ="/plan/planList")
+	@RequestMapping(value = "/plan/planList")
 	public String planList(Model model) {
-		
+
 		return "user/plan/planList";
 	}
-	
-	@RequestMapping(value ="/plan/planForm")
+
+	@RequestMapping(value = "/plan/planForm")
 	public String planForm(Model model) {
-		
+
 		return "user/plan/planForm";
 	}
-	
-	@RequestMapping(value ="/plan/planView")
+
+	@RequestMapping(value = "/plan/planView")
 	public String planView(Model model) {
-		
+
 		return "user/plan/planView";
 	}
-	
-	@RequestMapping(value ="/plan/planEdit")
+
+	@RequestMapping(value = "/plan/planEdit")
 	public String planEdit(Model model) {
-		
+
 		return "user/plan/planEdit";
 	}
 }

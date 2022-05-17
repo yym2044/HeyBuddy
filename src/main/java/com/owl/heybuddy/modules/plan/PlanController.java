f81d37c949315787f8062a4a85cc63503730edfb
james <dlrjsdn53@naver.com>
2022-05-17 22:57:15 +0900

    일정

--- src/main/java/com/owl/heybuddy/modules/plan/PlanController.java
+++ src/main/java/com/owl/heybuddy/modules/plan/PlanController.java
@@ -1,5 +1,10 @@
 package com.owl.heybuddy.modules.plan;
 
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +12,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.owl.heybuddy.modules.member.Member;
 import com.owl.heybuddy.modules.plan.Plan;
 import com.owl.heybuddy.modules.plan.PlanVo;
 
@@ -17,11 +23,33 @@ public class PlanController {
 	PlanServiceImpl service;
 
 	@RequestMapping(value = "/plan/planList")
-	public String planList() throws Exception {
+	public String planList(@ModelAttribute("vo") PlanVo vo, Plan dto, Model model, HttpSession httpSession) throws Exception {
+		
+		
+		if(vo.getHyspSeq() != null) {
+			httpSession.setAttribute("hyspSeq", vo.getHyspSeq());
+		}
+		
+		vo.setHymmSeq((String) httpSession.getAttribute("sessSeq"));
+		vo.setHymmName((String) httpSession.getAttribute("sessName"));
+		vo.setHyspSeq((String) httpSession.getAttribute("hyspSeq"));
+		vo.setHyspName((String) httpSession.getAttribute("hyspName"));
+		
+		System.out.println("vo.getHymmSeq :" + vo.getHymmSeq());
+		System.out.println("vo.getHymmName :" + vo.getHymmName());
+		System.out.println("vo.getHyspSeq :" + vo.getHyspSeq());
+		System.out.println("vo.getHyspName :" + vo.getHyspName());
+		
+		
+		/* List<Plan> selectListSpace = service.selectListSpace(); */
+		
+		/* model.addAttribute("selectListSpace", selectListSpace); */
+	
 		
 		return "user/plan/planList";
 	}
 	
+	// 일정에는 폼이 필요없어 보이나 마감할 때 필요 없으면 제거 
 	@RequestMapping(value = "/plan/planForm")
 	public String planForm() throws Exception {
 
@@ -38,16 +66,18 @@ public class PlanController {
 		System.out.println("dto.gethyplDesc(): " + dto.getHyplDesc());
 		System.out.println("dto.gethyplMemberName(): " + dto.getHyplMemberName());
 		
-		int result = service.insert(dto);
+		
+		 /* int result = service.insert(dto); */
+		 
+		 /* System.out.println("result: " + result); */
 
-		System.out.println("result: " + result);
+		/* redirectAttributes.addFlashAttribute("dto", dto); */
 
 		vo.setHyplSeq(dto.getHyplSeq());
-		/* redirectAttributes.addFlashAttribute("dto", dto); */
 
 		redirectAttributes.addFlashAttribute("vo", vo);
 		
-		return "redirect:user/plan/planList";
+		return "redirect:user/plan/planView";
 	}
 	
 	@RequestMapping(value = "/plan/planView")

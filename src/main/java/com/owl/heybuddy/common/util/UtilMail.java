ad27af5a4ce673b7b75242e656b8954376ceb5e7
yym2044 <yym2044@gmail.com>
2022-05-26 17:04:04 +0900

    UtilMail & pom

---
+++ src/main/java/com/owl/heybuddy/common/util/UtilMail.java
@@ -0,0 +1,53 @@
+package com.owl.heybuddy.common.util;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class UtilMail {
+
+	public static void sendMail() {
+		
+		final String username = "지메일주소";
+        final String password = "카피했던것";
+
+        Properties prop = new Properties();
+		prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put("mail.smtp.ssl.protocols", "TLSv1.2");
+        
+        Session session = Session.getInstance(prop,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("보내는사람 이메일"));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse("받는사람1, 받는사람2")
+            );
+            message.setSubject("Testing Gmail TLS");
+            message.setText("Dear Mail Crawler,"
+                    + "\n\n Please do not spam my email!");
+
+            Transport.send(message);
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+	    
+	}	
+}
